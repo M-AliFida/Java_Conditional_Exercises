bfc68afac92ef794ab860231d32a84477ac0626a
mfida <57366112+mfida@users.noreply.github.com>
2022-01-06 21:36:44 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,12 +29,33 @@ public class Main {
 //        test_results.biology_result= 50;
 //        test_results.displayPercentage();
 
-//        Flowchart1 flowchart_one = new Flowchart1();
+//        FlowChart1 flowchart_one = new FlowChart1();
 //        int result = flowchart_one.sumOrProduct(1, 2, false);
 //        System.out.println(result);
 
-        Flowchart2 flowchart_two = new Flowchart2();
-        flowchart_two.flowchart(2000);
+//        FlowChart2 flowchart_two = new FlowChart2();
+//        flowchart_two.flowchart(2000);
+
+//        FlowChart3 flowChart3 = new FlowChart3();
+//        flowChart3.flowChart(100);
+
+//        Blackjack blackjackResult = new Blackjack();
+//        blackjackResult.blackJack(22, 23);
+
+//        UniqueSum uniqueSumResult = new UniqueSum();
+//        uniqueSumResult.uniqueSum(1,2,1);
+
+//        TaxCalculator checkBracket = new TaxCalculator();
+//        double whatBracket = checkBracket.taxWhichBracket(20000);
+//        System.out.println(whatBracket);
+//
+//        TaxCalculator taxCalculate = new TaxCalculator();
+//        double taxCalculate_result = taxCalculate.taxCalculation(20000);
+//        System.out.println(taxCalculate_result);
+
+//        FizzBuzz fizzBuzzResult = new FizzBuzz();
+//        fizzBuzzResult.fizzBuzz(69);
+
 
 
 
